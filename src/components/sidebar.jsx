f5a938a95f294f03137f37b8cff641a7e3c7a3bf
Ben Finley <bben10hi@gmail.com>
2022-07-14 17:34:03 -0400

    created base layout for sidebar

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,56 @@
+import { MdOutlineContactMail, MdOutlineListAlt, MdOutlineFolderShared } from "react-icons/md";
+import { WiDayRainMix } from "react-icons/wi";
+import { BsGlobe2, BsLinkedin, BsGithub } from "react-icons/bs";
+
+const home = <MdOutlineListAlt size="40"/>;
+const projects = <MdOutlineFolderShared size="40"/>;
+const gis = <BsGlobe2 size="40"/>;
+const weather = <WiDayRainMix size="40"/>;
+const resume = <MdOutlineContactMail size="40"/>;
+const linkedin = <BsLinkedin size="40"/>;
+const github = <BsGithub size="40"/>;
+
+const Sidebar = () => {
+  return ( 
+    <div className="fixed top-0 left-0 h-screen w-36 m-0
+                    flex flex-col items-center
+                      bg-sideBarPrimary shadow-lg">
+      <div className="contents">
+        <SidebarIcon icon={home} pageName="Home" />
+        <SidebarIcon icon={projects} pageName="Coding Projects" />
+        <SidebarIcon icon={gis} pageName="GIS Projects" />
+        <SidebarIcon icon={weather} pageName="Whats The Weather" />
+        <SidebarIcon icon={resume} pageName="Resume" />
+      </div>
+      <div className="links mt-auto">
+      <SidebarLink icon={github} link="https://github.com/benfin75" />
+      <SidebarLink icon={linkedin} link="https://linkedin.com/in/benfin75" />
+      </div>
+    </div>
+   );
+}
+
+const SidebarIcon = ({ icon, pageName }) => {
+  return (
+    <div className="sidebar-icon group">
+      { icon }
+
+      <span className="sidebar-page-name group-hover:scale-100">
+        {pageName}
+      </span>
+    </div>
+  )
+}
+
+const SidebarLink = ({ icon, link }) => {
+  return (
+    <a className="sidebar-link 
+                text-sideBarSecondary hover:text-sideBarHover
+                p-2"
+                href={link} target="_blank" rel="noreferrer">
+      { icon }
+    </a>
+  )
+}
+
+export default Sidebar;
