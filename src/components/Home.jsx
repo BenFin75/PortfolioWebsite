2862620c0b0d99df79f5bc845a91d776084f48b1
Ben Finley <bben10hi@gmail.com>
2022-07-14 17:34:57 -0400

    created skeleton for the home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return ( 
+    <div className="home bg-mainPrimary flex-1">
+      <div className="intro">
+        <div className="blurb text-mainSecondary">
+          Hi, my name is Ben Finley!
+        </div>
+        <div className="headshot">
+          <div className="img h-20 w-20 bg-mainSecondary"></div>
+        </div>
+      </div>
+    </div>
+   );
+}
+ 
+export default Home;
