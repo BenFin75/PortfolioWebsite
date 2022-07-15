d96bf38e17fa26eb9d5da43ed444891d2488cc31
Ben Finley <bben10hi@gmail.com>
2022-07-15 12:54:51 -0400

    finished the functionality ofr the sidebar component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -10,17 +10,53 @@ const resume = <MdOutlineContactMail size="40"/>;
 const linkedin = <BsLinkedin size="40"/>;
 const github = <BsGithub size="40"/>;
 
-const Sidebar = () => {
+const Sidebar = ({ setCurrentPage }) => {
+
+  const changePage = (e) => {
+    document.querySelectorAll('.sidebar-icon').forEach(button => button.classList.remove('clicked'));
+    let buttonClicked;
+    if (e.target.tagName !== 'DIV') {
+      if (e.target.tagName !== 'svg') {
+        buttonClicked = e.target.parentNode.parentNode;
+      } else {
+        buttonClicked = e.target.parentNode;
+      }
+    } else {
+      buttonClicked = e.target;
+    }
+    buttonClicked.classList.add('clicked');
+
+    switch (buttonClicked.id) {
+      case 'Home' :
+        setCurrentPage('home');
+        break;
+      case 'Coding Projects' :
+        setCurrentPage('coding');
+        break;
+      case 'GIS Projects' :
+        setCurrentPage('gis');
+        break;
+      case 'Whats The Weather' :
+        setCurrentPage('weather');
+        break;
+      case 'Resume' :
+        setCurrentPage('resume');
+        break;
+      default :
+        break;
+    }
+  }
+
   return ( 
     <div className="fixed top-0 left-0 h-screen w-36 m-0
                     flex flex-col items-center
                       bg-sideBarPrimary shadow-lg">
       <div className="contents">
-        <SidebarIcon icon={home} pageName="Home" />
-        <SidebarIcon icon={projects} pageName="Coding Projects" />
-        <SidebarIcon icon={gis} pageName="GIS Projects" />
-        <SidebarIcon icon={weather} pageName="Whats The Weather" />
-        <SidebarIcon icon={resume} pageName="Resume" />
+        <SidebarIcon icon={home} pageName="Home" changePage={changePage}/>
+        <SidebarIcon icon={projects} pageName="Coding Projects" changePage={changePage} />
+        <SidebarIcon icon={gis} pageName="GIS Projects" changePage={changePage} />
+        <SidebarIcon icon={weather} pageName="Whats The Weather" changePage={changePage} />
+        <SidebarIcon icon={resume} pageName="Resume" changePage={changePage} />
       </div>
       <div className="links mt-auto">
       <SidebarLink icon={github} link="https://github.com/benfin75" />
@@ -30,9 +66,9 @@ const Sidebar = () => {
    );
 }
 
-const SidebarIcon = ({ icon, pageName }) => {
+const SidebarIcon = ({ icon, pageName, changePage }) => {
   return (
-    <div className="sidebar-icon group">
+    <div className="sidebar-icon group" id={pageName} onClick={(pageName) => changePage(pageName)}>
       { icon }
 
       <span className="sidebar-page-name group-hover:scale-100">
