173d428800d0154e25310dfe981aed9e6dd07b52
Ben Finley <bben10hi@gmail.com>
2022-07-15 12:55:35 -0400

    created the coding page to test sidebar functionality

---
+++ src/components/Coding.jsx
@@ -0,0 +1,10 @@
+const Coding = () => {
+  return ( 
+    <div className="coding flex">
+      <p className="telegram-bot"> Telegram Bot </p>
+      <p className="tablet-weaving">Tablet Weaving</p>
+    </div>
+   );
+}
+ 
+export default Coding;
