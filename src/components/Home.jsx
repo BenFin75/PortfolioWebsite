2594d1def542069532186e381c04199897cd014f
Ben Finley <bben10hi@gmail.com>
2022-07-25 12:40:33 -0400

    started designing the layout in flexbox

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,23 @@
 const Home = () => {
   return ( 
-    <div className="home bg-mainPrimary flex-1">
-      <div className="intro">
-        <div className="blurb text-mainSecondary">
-          Hi, my name is Ben Finley!
+    <div className="home bg-mainPrimary flex-1 flex-col">
+      <div className="intro flex-1 flex">
+        <div className="text-area">
+          <div className="name">
+            Hi, my name is Ben Finley!
+          </div>
+          <div className="intro-blurb text-mainSecondary">
+            I'm a web developer with a degree in geology. 
+          </div>
         </div>
         <div className="headshot">
           <div className="img h-20 w-20 bg-mainSecondary"></div>
+        </div>
+      </div>
+      <div className="education flex-1">
+        <div className="fieldShot">Image Here</div>
+        <div className="edu-blurb">
+          I recently completed the Odin Project bootcamp. I have a degree in geology, with significant experience in the GIS field.
         </div>
       </div>
     </div>
