2ef78baae811d453e5808c9652854e0c84820c08
Ben Finley <bben10hi@gmail.com>
2022-07-25 12:38:53 -0400

    added background color

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -1,6 +1,6 @@
 const Coding = () => {
   return ( 
-    <div className="coding flex">
+    <div className="coding flex bg-teal-700 flex-1">
       <p className="telegram-bot"> Telegram Bot </p>
       <p className="tablet-weaving">Tablet Weaving</p>
     </div>
