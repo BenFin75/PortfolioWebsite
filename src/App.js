4fe115682dd3bb741cbff686136d1345cfc47dcb
Ben Finley <bben10hi@gmail.com>
2022-07-25 12:39:58 -0400

    added a state for the current page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import {useState} from 'react';
 import Sidebar from './components/sidebar';
 import Home from './components/Home';
+import Coding from './components/Coding';
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('home')
   return (
     <div className="App flex">
-      <Sidebar />
-      <div className="main ml-36 flex">
-        <Home />
+      <Sidebar setCurrentPage={setCurrentPage} />
+      <div className="main ml-36 bg-teal-600 flex flex-1 min-h-screen">
+        {currentPage === 'home' && <Home />}
+        {currentPage === 'coding' && <Coding />}
       </div>
     </div>
   );
