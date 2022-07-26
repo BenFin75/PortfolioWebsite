945a4a4865af1cc612d87b40223cff8cf42b1748
Ben Finley <bben10hi@gmail.com>
2022-07-26 19:27:46 -0400

    added title

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -3,7 +3,7 @@ const Weather = () => {
     <div className="weather-page flex flex-1">
       <iframe id="inlineFrameExample"
               className="flex-grow"
-              title="Inline Frame Example"
+              title="My Weather App"
               src="https://benfin75.github.io/Weather-App/"
               frameBorder="0"
       ></iframe>
